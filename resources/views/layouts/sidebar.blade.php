92fde95018ea2cbf2f06cc7a2488afa0d28a3952
Adwi Arifin <adwiarifin@gmail.com>
2017-07-02 10:24:04 +0700

    Change the sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,13 @@
 <div class="col-sm-3 offset-sm-1 blog-sidebar">
-    <div class="sidebar-module sidebar-module-inset">
-        <h4>About</h4>
-        <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    <div class="sidebar-module">
+        <h4>Social Page</h4>
+        <ol class="list-unstyled">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+        </ol>
     </div>
+    
     <div class="sidebar-module">
         <h4>Archives</h4>
         <ol class="list-unstyled">
@@ -11,13 +16,6 @@
             @endforeach
         </ol>
     </div>
-    <div class="sidebar-module">
-        <h4>Elsewhere</h4>
-        <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
-        </ol>
-    </div>
+    
 </div>
 <!-- /.blog-sidebar -->
